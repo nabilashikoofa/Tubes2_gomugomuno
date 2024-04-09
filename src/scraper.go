0dedff9b34cd5d74c1246e47bb196dccded85a09
Nabila Shikoofa <nabilashikoofa@gmail.com>
2024-04-09 17:47:41 +0700

    Update scraper.go

--- src/scraper.go
+++ src/scraper.go
@@ -10,7 +10,12 @@ func main()	{
 
 	//Ubah class dari inspect
 	c.OnHTML("div.mw-page-container a[href^='/wiki/']", func(h *colly.HTMLElement) {
-		c.Visit(h.Request.AbsoluteURL(h.Attr("href")))
+		// nampilin semua link yag ada di halaman itu
+		link := h.Request.AbsoluteURL(h.Attr("href"))
+		fmt.Println(link) 
+
+		// yg ini buat masuk ke link baru
+		// c.Visit(h.Request.AbsoluteURL(h.Attr("href")))
 	})
 
 	c.OnRequest(func(r *colly.Request) {
@@ -18,6 +23,7 @@ func main()	{
 	})
 
 	//Ubah link webnya disini
-	c.Visit("https://id.wikipedia.org/wiki/Institut_Teknologi_Bandung")
+	c.Visit("https://id.wikipedia.org/wiki/Vektor")
+	//c.Visit("https://id.wikipedia.org/wiki/Institut_Teknologi_Bandung")
 	// c.Visit("https://id.wikipedia.org/wiki/Universitas_Indonesia")
 }
