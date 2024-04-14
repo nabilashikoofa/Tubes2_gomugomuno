70923bf205bb2d04e5e00128102cc967cee2c6f5
Nabila Shikoofa <nabilashikoofa@gmail.com>
2024-04-14 14:07:36 +0700

    Update scraper.go

--- src/backend/Scrape/scraper.go
+++ src/backend/Scrape/scraper.go
@@ -3,17 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/gocolly/colly"
+	"strings"
 )
 
 func main()	{
 	c := colly.NewCollector()
 
+	var links []string
+
 	//Ubah class dari inspect
 	c.OnHTML("div.mw-page-container a[href^='/wiki/']", func(h *colly.HTMLElement) {
-		// nampilin semua link yag ada di halaman itu
 		link := h.Request.AbsoluteURL(h.Attr("href"))
-		fmt.Println(link) 
+		link = strings.TrimPrefix(link, "https://en.wikipedia.org/wiki/")
 
+		if !contains(links, link) {
+			links = append(links, link)
+		}
 		// yg ini buat masuk ke link baru
 		// c.Visit(h.Request.AbsoluteURL(h.Attr("href")))
 	})
@@ -24,6 +29,15 @@ func main()	{
 
 	//Ubah link webnya disini
 	c.Visit("https://en.wikipedia.org/wiki/Graph_traversal")
-	//c.Visit("https://id.wikipedia.org/wiki/Institut_Teknologi_Bandung")
-	// c.Visit("https://id.wikipedia.org/wiki/Universitas_Indonesia")
+
+	fmt.Println(links)
+}
+
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+	return false
 }
