14c49a0dd8db87dc9b3f1ea9ae1b16ee930bc84a
Nabila Shikoofa <nabilashikoofa@gmail.com>
2024-04-23 23:16:21 +0700

    feat(GraphComponent):  add graph visualization

---
+++ src/frontend/app/src/GraphComponent/GraphComponent.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Graph } from "react-d3-graph";
+
+class GraphComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataArray: this.props.result
+        };
+}
+
+    // Fungsi untuk mengonversi array of array menjadi data graf
+    convertArrayToGraphData = (dataArray) => {
+        const nodes = [];
+        const links = [];
+
+        dataArray.forEach((array) => {
+            array.forEach((element, index) => {
+                if (!nodes.find((node) => node.id === element)) {
+                    nodes.push({ id: element });
+                }
+
+                if (index < array.length - 1) {
+                    links.push({ source: array[index], target: array[index + 1] });
+                }
+            });
+        });
+        return { nodes, links };
+    };
+
+    render() {
+        const { dataArray } = this.state;
+        const graphData = this.convertArrayToGraphData(dataArray);
+
+        // Konfigurasi graf
+        const myConfig = {
+            nodeHighlightBehavior: true,
+            directed: true,
+            node: {
+                color: "lightblue",
+                size: 120,
+                highlightStrokeColor: "blue",
+            },
+            link: {
+                highlightColor: "lightblue",
+            },
+        };
+
+        return (
+            <div>
+                <h1>Graph</h1>
+                <Graph
+                    id="graph-id" // id wajib
+                    data={graphData}
+                    config={myConfig}
+                />
+            </div>
+        );
+    }
+}
+
+export default GraphComponent;
