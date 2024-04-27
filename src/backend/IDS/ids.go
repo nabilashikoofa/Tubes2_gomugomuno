3643c69fd9eac123dc9b0a4916745bbb3ef9d87d
HanifIHM <imamhanif2003@gmail.com>
2024-04-27 17:50:36 +0700

    feat : ids paralel

--- src/backend/IDS/ids.go
+++ src/backend/IDS/ids.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 	"Tubes2_gomugomuno/Scrape"
+	"sync"
 	"time"
 )
 
@@ -119,4 +120,88 @@ func IDS(startTitle string, endTitle string)  ([][]string, int64, int, int, int)
 	return result, elapsed, iterasi, root.getSumAll(), len(result) 
 }
 
+func IDSParalel(startTitle string, endTitle string) ([][]string, int64, int, int, int) {
+	start := time.Now()
+	var result [][] string 
+	var wg sync.WaitGroup
+	maxNumThread := 300
+	pengali := 0
+	startTitle = Scrape.Convert(startTitle)
+	root := createTree(startTitle)
+	childRoot := Scrape.Scraper(startTitle)
+	numThreads := len(childRoot)
+	endTitle = Scrape.Convert(endTitle)
+	path := []string{startTitle}
+	sisa := numThreads
+	resultavailable := &result
+	fmt.Println(numThreads)
+	for _, child := range childRoot {
+		root.AddSubtree(child)
+		if strings.EqualFold(child, endTitle) {
+			*resultavailable = append(*resultavailable, []string{startTitle,child})
+		}
+	}
+	if numThreads > maxNumThread {
+		pengali = numThreads / maxNumThread
+		sisa = numThreads % maxNumThread
+	}
+	iterasi := 1
+	for len(*resultavailable) == 0 {
+		fmt.Println("Iterasi : ",iterasi)
+		for x := 0; x < pengali; x++ {
+			for i := 0; i < maxNumThread; i++ {
+				wg.Add(1)
+				go func(akar *Tree) {
+					defer wg.Done()
+					DLSParalel(akar, endTitle, path, iterasi, resultavailable)
+				}(root.SubTree[x*maxNumThread + i])
+			}
+			wg.Wait()
+		}
+		for r := 0; r < sisa; r++ {
+			wg.Add(1)
+				go func(akar *Tree) {
+					defer wg.Done()
+					DLSParalel(akar, endTitle, path, iterasi, resultavailable)
+				}(root.SubTree[pengali*maxNumThread + r])	
+		}
+		wg.Wait()
+		iterasi++
+	}
+	elapsed := time.Since(start).Milliseconds()
+	return *resultavailable, elapsed, iterasi, root.getSumAll(), len(result)
+}
 
+func DLSParalel(root *Tree, endUrl string, path []string, iterasi int, resultavailable *[][]string) {			
+	if iterasi == 1 {
+		hasil_scrape := Scrape.Scraper(root.Value)
+		if hasil_scrape == nil {
+			fmt.Println("gaada anak")
+			return
+		} else {
+			for _, sub := range hasil_scrape {
+				root.AddSubtree(sub)
+				// fmt.Println("visited " + sub)
+				if strings.EqualFold(sub, endUrl) {
+					fmt.Println("Ketemuuu pathnya", iterasi)
+					path := append(path,root.Value, sub)
+					fmt.Println(path)
+					// *resultavailable = append(*resultavailable, path)
+					*resultavailable = [][]string{path} 
+					return 
+				}					
+			} 
+			return
+		}
+	} else {
+		path = append(path, root.Value)
+		for _, child := range root.SubTree {
+			// fmt.Println(child.Value)
+			DLSParalel(child, endUrl, path, iterasi-1, resultavailable)
+			if len(*resultavailable) > 0 {
+				break
+			}
+		}
+		return
+	}
+}
