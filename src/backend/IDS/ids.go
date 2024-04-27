1dbaa5dc88505b792a0f9c8ce154c1565f668600
HanifIHM <imamhanif2003@gmail.com>
2024-04-27 21:24:30 +0700

    fix ids

--- src/backend/IDS/ids.go
+++ src/backend/IDS/ids.go
@@ -6,24 +6,34 @@ import (
 	"Tubes2_gomugomuno/Scrape"
 	"sync"
 	"time"
+	"context"
 )
 
+// Struktur data Pohon
 type Tree struct {
 	Value string
 	SubTree []*Tree
 }
-
+// Membuat Pohon tanpa anak
 func createTree(startTitle string) *Tree{
 	return &Tree{
 		Value: startTitle,
 		SubTree: nil,
 	}
 }
-
 // Membuat anakan pohon
 func (t *Tree) AddSubtree(value string) {
 	child := &Tree{Value: value}
 	t.SubTree = append(t.SubTree, child)
+}
+// Mencari banyak pohon 
+func (t *Tree) getSumAll() int {
+	sum := 1
+	for _, sub := range t.SubTree {
+		sumSub := sub.getSumAll()
+		sum = sum + sumSub
+	}
+	return sum
 }
 
 func (t *Tree) displayTree(){
@@ -45,82 +55,11 @@ func (t *Tree) displayTreeWithLevel(level int){
 		sub.displayTreeWithLevel(level+1)
 	}
 }
-func (t *Tree) getSumAll() int {
-	sum := 1
-	for _, sub := range t.SubTree {
-		sumSub := sub.getSumAll()
-		sum = sum + sumSub
-	}
-	return sum
-}
-
-
-// DFS tapi dilimit di level tertentu
-func DLS(root *Tree, endUrl string, path []string, iterasi int) [][]string {
-	fmt.Println(path)
-	if strings.EqualFold(root.Value, endUrl) {
-		fmt.Println("masuk atas", iterasi)
-		return [][]string{append(path, root.Value)}
-	} else if iterasi == 1 {
-		hasil_scrape := Scrape.Scraper(root.Value)
-		if hasil_scrape == nil {
-			fmt.Println("masuk nil")
-			return [][]string{}
-		} else {
-			for _, sub := range hasil_scrape {
-				root.AddSubtree(sub)
-				fmt.Println("visited " + sub)
-				if strings.EqualFold(sub, endUrl) {
-					fmt.Println("masuk dalam", iterasi)
-					paths := append(path,root.Value, sub)
-					fmt.Println(paths)
-					return [][]string{paths}
-				}
-			}
-			return [][]string{}
-		}
-	} else {
-		path = append(path, root.Value)
-		var paths [][]string
-		for _, child := range root.SubTree {
-			fmt.Println(child.Value)
-			childPaths := DLS(child, endUrl, path, iterasi-1)
-			paths = append(paths, childPaths...)
-			if len(paths) > 0 {
-				break
-			}
-		}
-		return paths
-	}
-}
-
-func IDS(startTitle string, endTitle string)  ([][]string, int64, int, int, int) {
-	start := time.Now()
-	isKetemu := false
-	iterasi := 1
-	startUrl := Scrape.Convert(startTitle)
-	endUrl := Scrape.Convert(endTitle)
-	root := createTree(startUrl)
-	result := [][]string{}
-	for !isKetemu {
-		fmt.Println(iterasi)
-		root.displayTree()
-		result = DLS(root, endUrl, []string{}, iterasi)
-		if len(result) != 0 {
-			isKetemu = true
-		} else {
-			iterasi++
-		}
-		fmt.Println("hasil = ", result)
-	}
-	// root.displayTreeWithLevel(0);
-	elapsed := time.Since(start).Milliseconds()
-	fmt.Println(root.getSumAll())
-	fmt.Println("Algorithm execution time:", elapsed, "ms")
-	return result, elapsed, iterasi, root.getSumAll(), len(result) 
-}
 
 func IDSParalel(startTitle string, endTitle string) ([][]string, int64, int, int, int) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+    defer cancel() // Cancel context to release resources when done
+
 	start := time.Now()
 	var result [][] string 
 	var wg sync.WaitGroup
@@ -131,6 +70,7 @@ func IDSParalel(startTitle string, endTitle string) ([][]string, int64, int, int
 	childRoot := Scrape.Scraper(startTitle)
 	numThreads := len(childRoot)
 	endTitle = Scrape.Convert(endTitle)
+	visited := make(map[string]bool)
 	path := []string{startTitle}
 	sisa := numThreads
 	resultavailable := &result
@@ -147,13 +87,16 @@ func IDSParalel(startTitle string, endTitle string) ([][]string, int64, int, int
 	}
 	iterasi := 1
 	for len(*resultavailable) == 0 {
+		if time.Since(start).Minutes() > 5{
+			break
+		}
 		fmt.Println("Iterasi : ",iterasi)
 		for x := 0; x < pengali; x++ {
 			for i := 0; i < maxNumThread; i++ {
 				wg.Add(1)
 				go func(akar *Tree) {
 					defer wg.Done()
-					DLSParalel(akar, endTitle, path, iterasi, resultavailable)
+					DLSParalel(akar, endTitle, path, iterasi, resultavailable, visited, ctx)
 				}(root.SubTree[x*maxNumThread + i])
 			}
 			wg.Wait()
@@ -162,46 +105,56 @@ func IDSParalel(startTitle string, endTitle string) ([][]string, int64, int, int
 			wg.Add(1)
 				go func(akar *Tree) {
 					defer wg.Done()
-					DLSParalel(akar, endTitle, path, iterasi, resultavailable)
+					DLSParalel(akar, endTitle, path, iterasi, resultavailable, visited, ctx)
 				}(root.SubTree[pengali*maxNumThread + r])	
 		}
 		wg.Wait()
 		iterasi++
 	}
+	// root.displayTreeWithLevel(0)
 	elapsed := time.Since(start).Milliseconds()
 	return *resultavailable, elapsed, iterasi, root.getSumAll(), len(result)
 }
 
-func DLSParalel(root *Tree, endUrl string, path []string, iterasi int, resultavailable *[][]string) {			
-	if iterasi == 1 {
-		hasil_scrape := Scrape.Scraper(root.Value)
-		if hasil_scrape == nil {
-			fmt.Println("gaada anak")
-			return
-		} else {
-			for _, sub := range hasil_scrape {
-				root.AddSubtree(sub)
-				// fmt.Println("visited " + sub)
-				if strings.EqualFold(sub, endUrl) {
-					fmt.Println("Ketemuuu pathnya", iterasi)
-					path := append(path,root.Value, sub)
-					fmt.Println(path)
-					// *resultavailable = append(*resultavailable, path)
-					*resultavailable = [][]string{path} 
-					return 
-				}					
-			} 
-			return
-		}
-	} else {
-		path = append(path, root.Value)
-		for _, child := range root.SubTree {
-			// fmt.Println(child.Value)
-			DLSParalel(child, endUrl, path, iterasi-1, resultavailable)
-			if len(*resultavailable) > 0 {
-				break
-			}
-		}
+func DLSParalel(root *Tree, endUrl string, path []string, iterasi int, resultavailable *[][]string, visited map[string]bool, ctx context.Context) {	
+	select{
+	case <- ctx.Done():
 		return
+	default:
+		if len(*resultavailable) > 0 {
+			return
+		}		
+		if iterasi == 1 {
+			hasil_scrape := Scrape.Scraper(root.Value)
+			if hasil_scrape == nil {
+				fmt.Println("gaada anak")
+				return
+			} else {
+				for _, sub := range hasil_scrape {
+					if !visited[sub] {
+						root.AddSubtree(sub)
+						// fmt.Println("visited " + sub)
+						if strings.EqualFold(sub, endUrl) {
+							fmt.Println("Ketemuuu pathnya", iterasi)
+							path := append(path,root.Value, sub)
+							fmt.Println(path)
+							// *resultavailable = append(*resultavailable, path)
+							*resultavailable = [][]string{path} 
+							return 
+						}
+					}					
+				} 
+				return
+			}
+		} else {
+			path = append(path, root.Value)
+			for _, child := range root.SubTree {
+				DLSParalel(child, endUrl, path, iterasi-1, resultavailable, visited, ctx)
+				if len(*resultavailable) > 0 {
+					break
+				}
+			}
+			return
+		}
 	}
 }
