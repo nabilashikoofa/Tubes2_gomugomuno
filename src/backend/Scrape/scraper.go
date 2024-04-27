4d63587013fc84b703473cef9dfab04da8811be6
HanifIHM <imamhanif2003@gmail.com>
2024-04-27 11:19:09 +0700

    fix scrapper

--- src/backend/Scrape/scraper.go
+++ src/backend/Scrape/scraper.go
@@ -1,30 +1,28 @@
 package Scrape
 
 import (
-	"github.com/gocolly/colly"
 	"strings"
+    
+	"github.com/gocolly/colly"
 )
 
 func Scraper(title string) []string {
 	c := colly.NewCollector()
 
     linksMap := make(map[string]bool) // Map to store links
-
+    result := []string{}
     title = Convert(title)
     c.OnHTML("div.mw-page-container a[href^='/wiki/']", func(h *colly.HTMLElement) {
         link := h.Request.AbsoluteURL(h.Attr("href"))
         link = strings.TrimPrefix(link, "https://en.wikipedia.org/wiki/")
 
-        if !strings.ContainsAny(link, ":/%") && link != Convert(title) {
+        if !strings.ContainsAny(link, ":/%") && link != Convert(title) && !linksMap[link]{
             linksMap[link] = true // Store link in map
+            result = append(result, link)
         }
     })
 
     c.Visit("https://en.wikipedia.org/wiki/" + title)
-    var result []string
-    for link := range linksMap {
-        result = append(result, link)
-    }
     return result
 }
 
